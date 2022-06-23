437f8e43dea5295bbdae3038ef7dda3c15a9a34b
Andrei <andrei.parchukov@t-systems.com>
2022-06-24 01:17:42 +0300

    init

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	// MARK: - IB Outlets
+	@IBOutlet var paletteView: UIView!
+	
+	@IBOutlet var redValueLabel: UILabel!
+	@IBOutlet var greenValueLabel: UILabel!
+	@IBOutlet var blueValueLabel: UILabel!
+	
+	@IBOutlet var redSlider: UISlider!
+	@IBOutlet var greenSlider: UISlider!
+	@IBOutlet var blueSlider: UISlider!
+	
+	// MARK: - Life Cycles Methods
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		setupSliders()
+		setupLabels()
 	}
-
-
+	
+	override func viewDidLayoutSubviews() {
+		paletteView.layer.cornerRadius = 16
+		paletteView.layer.borderColor = UIColor.gray.cgColor
+		paletteView.layer.borderWidth = 1
+		setPaletteBackground()
+	}
+	
+	
+	// MARK: - IB Actions
+	@IBAction func redSliderChanged() {
+		redValueLabel.text = String(format: "%.2f", redSlider.value)
+		setPaletteBackground()
+	}
+	
+	@IBAction func greenSliderChanged() {
+		greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+		setPaletteBackground()
+	}
+	
+	@IBAction func blueSliderChanged() {
+		blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+		setPaletteBackground()
+	}
+	
+	// MARK: - Private Methods
+	private func setPaletteBackground() {
+		paletteView.backgroundColor = .init(
+			red: CGFloat(redSlider.value),
+			green: CGFloat(greenSlider.value),
+			blue: CGFloat(blueSlider.value),
+			alpha: 1.0
+		)
+	}
+	
+	private func setupSliders() {
+		redSlider.value = Float.random(in: 0...1.0)
+		greenSlider.value = Float.random(in: 0...1.0)
+		blueSlider.value = Float.random(in: 0...1.0)
+	}
+	
+	private func setupLabels() {
+		redValueLabel.text = String(format: "%.2f", redSlider.value)
+		greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+		blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+	}
+	
 }
 
